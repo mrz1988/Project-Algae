56608f1b20b9e7b2db5b21396a0d3849e40c6ae9
Matt Zychowski <matt@matts-mbp.acuitylightinggroup.com>
2016-10-12 18:11:46 -0400

    Add reformed tests for XUnit

---
+++ ZMath.Algebraic.Tests/Expressions/SymbolTypeTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Xunit;
+
+namespace ZMath.Algebraic.Tests
+{
+	public static class SymbolTypeTests
+	{
+		[Fact]
+		public static void AllSymbolTypesAreCategorized()
+		{
+			foreach (SymbolType type in Enum.GetValues(typeof(SymbolType)))
+			{
+				var isCategorized = type.IsValue() ||
+					type.IsUnaryOperation() ||
+					type.IsBinaryOperation() ||
+					type.IsDirectiveSymbol();
+				Assert.True(isCategorized);
+			}
+		}
+
+		[Fact]
+		public static void AllSymbolTypesHaveOrder()
+		{
+			foreach (SymbolType type in Enum.GetValues(typeof(SymbolType)))
+			{
+				type.Order();
+			}
+		}
+
+		[Fact]
+		public static void AdditionAndSubtractionHaveSameOrder()
+		{
+			Assert.Equal(SymbolType.Addition.Order(), SymbolType.Subtraction.Order());
+		}
+
+		[Fact]
+		public static void DivisionAndMultiplicationHaveSameOrder()
+		{
+			Assert.Equal(SymbolType.Multiplication.Order(), SymbolType.Division.Order());
+		}
+
+		[Fact]
+		public static void MultiplicationTrumpsAddition()
+		{
+			Assert.True(SymbolType.Multiplication.Order() > SymbolType.Addition.Order());
+		}
+
+		[Fact]
+		public static void ExponentiationTrumpsMultiplication()
+		{
+			Assert.True(SymbolType.Exponentiation.Order() > SymbolType.Multiplication.Order());
+		}
+
+		[Fact]
+		public static void AllUnaryOperationsHaveSameOrder()
+		{
+			var order = SymbolType.Negation.Order();
+
+			foreach (SymbolType type in Enum.GetValues(typeof(SymbolType)))
+			{
+				if (type.IsUnaryOperation())
+				{
+					Assert.Equal(order, type.Order());
+				}
+			}
+		}
+
+		[Fact]
+		public static void UnaryOperationsHavePrecedence()
+		{
+			Assert.True(SymbolType.Negation.Order() > SymbolType.Exponentiation.Order());
+		}
+	}
+}
