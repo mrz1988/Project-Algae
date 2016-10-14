2e544a2bfea9cdcc213817b37245975e5abd1a78
Matt Zychowski <matt@Matts-MacBook-Pro.local>
2016-10-14 10:56:54 -0400

    Add ZUtils and AsymmetricPipe

---
+++ ZUtils/Pipes/AsymmetricPipe.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ZUtils.Pipes
+{
+	public abstract class AsymmetricPipe<I, O> : IEnumerable<O>
+	{
+		private readonly IEnumerator<I> _input;
+		private Queue<O> _output;
+
+		public AsymmetricPipe(IEnumerable<I> input)
+		{
+			_input = input.GetEnumerator();
+		}
+
+		public abstract void Consume(I val);
+
+		private bool Pull()
+		{
+			while (_output.Count == 0)
+			{
+				if (!_input.MoveNext())
+					return false;
+				Consume(_input.Current)
+			}
+			return true;
+		}
+
+		public IEnumerator<O> GetEnumerator()
+		{
+			while (_output.Count > 0 || Pull())
+			{
+				yield return _output.Dequeue();
+			}
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return GetEnumerator();
+		}
+	}
+}
