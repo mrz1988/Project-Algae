00fd667da8db81807467c1379074715f86535527
Matt Zychowski <matt@matts-mbp.acuitylightinggroup.com>
2016-10-17 17:54:00 -0400

    Add high level support for substituting variables and evaluating

--- ZMath.Algebraic/Expressions/SymbolTree.cs
+++ ZMath.Algebraic/Expressions/SymbolTree.cs
@@ -4,15 +4,22 @@ namespace ZMath.Algebraic
 	public class SymbolTree
 	{
 		private ISymbol _root;
+		private VariableContext _ctx;
 
-		public SymbolTree(ISymbol root)
+		public SymbolTree(ISymbol root, VariableContext ctx)
 		{
 			_root = root;
+			_ctx = ctx;
+		}
+
+		public void Substitute(string variableName, Number substitution)
+		{
+			_ctx.Define(variableName, substitution);
 		}
 
 		public Number Evaluate()
 		{
-			return _root.GetValue();
+			return _root.MakeSubstitutions(_ctx).GetValue();
 		}
 	}
 }
